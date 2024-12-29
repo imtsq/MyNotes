2b25e6a40e888fe8f1786d3c4a9d76a6473208d2
imtsq <tagtsq@gmail.com>
2024-12-29 12:02:13 +0800

    Initial commit

--- 快速上传.bat
+++ 快速上传.bat
@@ -1,11 +1,11 @@
 git init
-git remote add origin git@github.com:imtsq/MyNotes.git
+git remote add origin git@gitee.com:imtsq/MyNotes.git
 git add .
 git branch -M main
 git commit -m "Initial commit"
 git push -f origin main
 
 # 更改远程仓库地址为 Gitee
-git remote set-url origin git@gitee.com:imtsq/MyNotes.git
+git remote set-url origin git@github.com:imtsq/MyNotes.git
 git push -f origin main
 pause
